29194afeb8744c4d5774ebc10cfdbcb7fe643d74
surf-guy <phong1815@gmail.com>
2022-04-01 14:34:29 -0700

    first upload on branch

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,42 @@
+var computer=0
+var human=0
+var tie=0
+
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent="hajime!!"
+container.appendChild(content);
+const buttons = document.querySelectorAll('button');
 
 
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
 
+    button.addEventListener('click', () => {
+      playRound(button.id,computerPlay())
+      content.textContent="Computer:"+computer+" Human:"+human;
+      container.appendChild(content);
+    });
+  }
+);
+
+if(computer==5 || human==5){
+  alert('game over!!!')
+}
+
+
+/* buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  //button.addEventListener('click', playRound(button.id,computerSelection));
+button.addEventListener('click', () => {
+   computer++
+   runningScore.textContent="Computer: "+computer+" "+"Player: "+human
+   container.appendChild(runningScore)
+  });
+});
+ */
 function computerPlay(){
     var choices = ["Rock","Paper","Scissors"];
     var choice = choices[Math.floor(Math.random() * choices.length)];
@@ -8,63 +44,97 @@ function computerPlay(){
 }
 
 function playRound(playerSelection, computerSelection) {
-    //console.log(computerSelection)
     if(computerSelection=="paper" && playerSelection=="rock"){
+     alert("You lose this round! "+computerSelection+" beats "+playerSelection)
+     computer++
+     return 'computer'
+    }
+
+    else if(computerSelection=="rock" && playerSelection=="rock"){
+      alert("Tied this round! "+computerSelection+"  tied with "+playerSelection)
+    }
+    else if(computerSelection=="scissors" && playerSelection=="rock"){
+     alert("You won this round! "+playerSelection+"  beats "+computerSelection)
+     human++
+     return 'human'
+     
+    }
+    else if(computerSelection=="rock" && playerSelection=="scissors"){
       alert("You lose this round! "+computerSelection+" beats "+playerSelection)
+      computer++
+      return "computer"    
+    }
+
+    else if(computerSelection=="scissors" && playerSelection=="scissors"){
+      alert("Tied this round! "+computerSelection+"  tied with "+playerSelection)
+
+    }
+    else if(computerSelection=="paper" && playerSelection=="scissors"){
+      alert("You won this round! "+playerSelection+"  beats "+computerSelection)
+      human++
+      return "human"
+    }
+    else if(computerSelection=="scissors" && playerSelection=="paper"){
+      alert("You lose this round! "+computerSelection+" beats "+playerSelection)
+      computer++
+      return "computer"    
+    }
+
+    else if(computerSelection=="paper" && playerSelection=="paper"){
+      alert("Tied this round! "+computerSelection+"  tied with "+playerSelection)
+    }
+    else if(computerSelection=="rock" && playerSelection=="paper"){
+      alert("You won this round! "+playerSelection+"  beats "+computerSelection)
+      human++
+      return "human"
+    }
+  }
+
+
+  /* function updateCount(playerSelection, computerSelection) {
+   
+    if(computerSelection=="paper" && playerSelection=="rock"){
       computer++
       return computer    
     }
 
     else if(computerSelection=="rock" && playerSelection=="rock"){
-      alert("Tied this round! "+computerSelection+"  tied with "+playerSelection)
       tie++
-      return tie
-      //return "Tie! "+computerSelection+" tie with "+playerSelection
+
     }
     else if(computerSelection=="scissors" && playerSelection=="rock"){
-      alert("You won this round! "+playerSelection+"  beats "+computerSelection)
       human++
       return human
     }
     else if(computerSelection=="rock" && playerSelection=="scissors"){
-      alert("You lose this round! "+computerSelection+" beats "+playerSelection)
       computer++
       return computer    
     }
 
     else if(computerSelection=="scissors" && playerSelection=="scissors"){
-      alert("Tied this round! "+computerSelection+"  tied with "+playerSelection)
       tie++
-      return tie
-      //return "Tie! "+computerSelection+" tie with "+playerSelection
     }
     else if(computerSelection=="paper" && playerSelection=="scissors"){
-      alert("You won this round! "+playerSelection+"  beats "+computerSelection)
       human++
       return human
     }
     else if(computerSelection=="scissors" && playerSelection=="paper"){
-      alert("You lose this round! "+computerSelection+" beats "+playerSelection)
       computer++
       return computer    
     }
 
     else if(computerSelection=="paper" && playerSelection=="paper"){
-      alert("Tied this round! "+computerSelection+"  tied with "+playerSelection)
       tie++
-      return tie
-      //return "Tie! "+computerSelection+" tie with "+playerSelection
+ 
     }
     else if(computerSelection=="rock" && playerSelection=="paper"){
-      alert("You won this round! "+playerSelection+"  beats "+computerSelection)
       human++
       return human
     }
-      //return "You win! "+computerSelection+" doesn't beat "+playerSelection
-  }
+  } */
 
 function game(){
-  for(var i=0; i<5; i++){
+  
     var playerSelection=''
     var inputValid=false
     while(inputValid===false){
@@ -77,7 +147,7 @@ function game(){
            inputValid=false
            alert(playerSelection+" "+inputValid)
        }
-    }
+    
     const computerSelection = computerPlay();
     playRound(playerSelection, computerSelection)
   }
